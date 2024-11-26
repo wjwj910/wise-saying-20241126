43ac9277b7ef362da4e0fdb6c2a34636bca46cbd
wjwj910 <wjwj910@gmail.com>
2024-11-26 15:27:29 +0900

    t5 BLUE

--- src/main/java/com/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/domain/wiseSaying/controller/WiseSayingController.java
@@ -12,5 +12,7 @@ public class WiseSayingController {
     public void actionAdd() {
         System.out.println("명언 : ");
         System.out.println("작가 : ");
+
+        System.out.println("1번 명언이 등록되었습니다.");
     }
 }
