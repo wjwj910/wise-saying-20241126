65016a7b5d6d6e90594d1973bed2ed6338b2faa3
wjwj910 <wjwj910@gmail.com>
2024-11-26 15:30:36 +0900

    t6 BLUE

--- src/main/java/com/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class WiseSayingController {
     private final Scanner scanner;
-
+    private int lastId;
     public WiseSayingController(Scanner scanner) {
         this.scanner = scanner;
+        this.lastId = 0;
     }
 
     public void actionAdd() {
         System.out.println("명언 : ");
         System.out.println("작가 : ");
 
-        System.out.println("1번 명언이 등록되었습니다.");
+        int id = ++lastId;
+        System.out.println(id +"번 명언이 등록되었습니다.");
+
     }
 }
