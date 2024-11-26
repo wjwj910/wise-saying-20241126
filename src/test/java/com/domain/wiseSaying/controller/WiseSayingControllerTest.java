1bd46a29a20b101bd8e54ab3080ad0e548687308
wjwj910 <wjwj910@gmail.com>
2024-11-26 15:25:38 +0900

    t5 RED

--- src/test/java/com/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -20,4 +20,17 @@ public class WiseSayingControllerTest {
                 .contains("명언 : ")
                 .contains("작가 : ");
     }
+
+    @Test
+    @DisplayName("등록이 완료되면 명언 번호가 출력된다.")
+    public void t5() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("1번 명언이 등록되었습니다.");
+    }
 }
