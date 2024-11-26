c81ac70586b75397c0299f2ff78ac8fe41eeec2c
wjwj910 <wjwj910@gmail.com>
2024-11-26 12:37:44 +0900

    work

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,14 +1,16 @@
 package com;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //lab1();
-        lab2();
+        //lab2();
+        //lab3();
+        lab4();
     }
+
 
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
@@ -32,5 +34,32 @@ public class Main {
         cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab3() {
+        System.out.println("안녕하세요");
+    }
+
+    private static void lab4() {
+        System.out.println("== 시작 ==");
+        // 출력이 모니터로 안나오게 설정
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        
+        //output에 쌓임
+        System.out.print("안녕하세요.");
+        System.out.println("반갑습니다.");
+
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        System.out.println("== 끝 ==");
+        System.out.println("출력 : " + output.toString());
+        // output 자원 해제
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
