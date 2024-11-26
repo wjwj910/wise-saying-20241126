da61126b29f8ffd56d18f49c212a3a4759f09139
wjwj910 <wjwj910@gmail.com>
2024-11-26 14:23:50 +0900

    fail work

---
+++ src/main/java/com/App/App.java
@@ -0,0 +1,15 @@
+package com.App;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+    }
+}
--- src/test/java/com/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,5 +1,6 @@
 package com.domain.wiseSaying.controller;
 
+import com.App.App;
 import com.standard.util.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,20 +11,40 @@ import java.util.Scanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingControllerTest {
-
     @Test
     @DisplayName("== 명언 앱 ==")
-    public void t1(){
+    public void t1() {
         Scanner scanner = TestUtil.getScanner("종료");
         ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
 
         App app = new App(scanner);
         app.run();
 
+        String output = outputStream.toString();
+
         TestUtil.clearSetOutToByteArray(outputStream);
 
-        assertThat(outputStream.toString())
+        assertThat(output)
                 .contains("== 명언 앱 ==");
+    }
 
+    @Test
+    @DisplayName("명령) ")
+    public void t2() {
+        Scanner scanner = TestUtil.getScanner("""
+                목록
+                종료
+                """);
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output)
+                .contains("명령) ");
     }
 }
