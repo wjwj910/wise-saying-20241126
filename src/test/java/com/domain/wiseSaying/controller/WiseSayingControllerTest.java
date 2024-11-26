c00a34260a17c1718895e9208f508b94ad463df4
wjwj910 <wjwj910@gmail.com>
2024-11-26 15:34:59 +0900

    ãt7 RED

--- src/test/java/com/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -50,4 +50,23 @@ public class WiseSayingControllerTest {
                 .contains("1번 명언이 등록되었습니다.")
                 .contains("2번 명언이 등록되었습니다.");
     }
+
+    @Test
+    @DisplayName("목록 명령어 : 입력된 명언들을 출력한다.")
+    public void t7() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("번호 / 작가 / 명언")
+                .contains("-------------------")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
