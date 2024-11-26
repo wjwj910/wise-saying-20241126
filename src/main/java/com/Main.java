26bac88e3b32eeeffe0f393127e209f4d0b9ff27
wjwj910 <wjwj910@gmail.com>
2024-11-26 12:24:40 +0900

    work

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,17 +1,18 @@
 package com;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        lab1();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void lab1() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("명령)");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력한 명령: " + cmd);
     }
 }
