7d79681eb2d93e1e4ca74ae25eed195f939c9965
wjwj910 <wjwj910@gmail.com>
2024-11-26 12:29:01 +0900

    work

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,17 +1,35 @@
 package com;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        //lab1();
+        lab2();
     }
 
-    public static void lab1() {
+    private static void lab1() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("명령)");
         String cmd = scanner.nextLine();
+
+        System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab2(){
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
+        Scanner scanner = new Scanner(in);
+
+        System.out.print("명령)");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+
+        System.out.print("명령)");
+        cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
     }
